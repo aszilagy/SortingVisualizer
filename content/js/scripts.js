94f01ec07d707844234c9b0c9bc094dc770d0474
DESKTOP-5J2VUUC\Alex <aszilagy@hawk.iit.edu>
2020-01-24 12:55:06 -0600

    content changes

--- content/js/scripts.js
+++ content/js/scripts.js
@@ -19,6 +19,10 @@ slider.oninput = function(){
         loadGraph();
 }
 
+function getBars(){
+        return bars;
+}
+
 function loadGraph(){
         bars = [];
         output.innerHTML = slider.value;
@@ -31,7 +35,7 @@ function loadGraph(){
 }
 
 function clearCanvas(){
-        ctx.fillStyle = "#FF0000";
+        ctx.fillStyle = "#000000";
         ctx.clearRect(0, 0, cvWidth, cvHeight);
 
         canvasTextBox.clearRect(0, 0, cvTextBox.width, cvTextBox.height);
@@ -53,13 +57,48 @@ function drawBars(bars){
         y = cvHeight - sizeOfBar
         width = 5
         height = sizeOfBar*/
-
+        clearCanvas();
         for(var i=0; i<bars.length; i++){
                 //x, y, width, height
-                ctx.fillRect(i*50, cvHeight-bars[i], 5, bars[i]);
+                ctx.fillRect(i*50, cvHeight-(bars[i]*3), 5, bars[i]*3);
                 canvasTextBox.fillText(bars[i]/10, (i*50)+10, cvTextBox.height/2);
         }
         
+}
+
+function animate(bars){
+        return;
+}
+
+function merge(left, right){
+        let sortedArray = [], leftIndex = 0, rightIndex = 0;
+
+        while(leftIndex < left.length && rightIndex < right.length){
+                if(left[leftIndex] < right[rightIndex]){
+                        sortedArray.push(left[leftIndex]);
+                        leftIndex++;
+                }
+                else{
+                        sortedArray.push(right[rightIndex]);
+                        rightIndex++;
+                }
+        }
+
+        return sortedArray.concat(left.slice(leftIndex).concat(right.slice(rightIndex)));
+}
+
+function mergeSort(arr){
+        
+        if(arr.length < 2){
+                return arr;
+        }
+
+        const middle = Math.floor(arr.length / 2);
+
+        const left = arr.slice(0, middle);
+        const right = arr.slice(middle);
+        
+        return merge(mergeSort(left), mergeSort(right));
 }
 
 
